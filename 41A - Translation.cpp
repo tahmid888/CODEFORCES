8085a6f3fbff73c3d147c48e72af06fe75b8b778
tahmid888 <76845333+tahmid888@users.noreply.github.com>
2022-02-03 03:01:07 +0600

    Add files via upload

---
+++ 41A - Translation.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s,t;
+    cin>>s;
+   cin>>t;
+
+  reverse(t.begin() , t.end());
+  if(s==t)
+  cout<<"YES";
+  else
+  cout<<"NO";
+
+
+    return 0;
+}
