e0d3470a54f6607f35bcab2e87d5fbebbe6a03ad
tahmid888 <76845333+tahmid888@users.noreply.github.com>
2022-02-05 16:44:48 +0600

    Add files via upload

---
+++ 467A - George and Accommodation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int n,p,q,count=0,coop=0;
+   cin>>n;
+   for(int i=0;i<n;i++)
+   {
+       cin>>p>>q;
+
+       if(q-p>=2)
+       count++;
+
+
+   }
+   cout<<count<<endl;
+    return 0;
+}
