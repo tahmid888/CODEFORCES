6b4563e3ea8496fa7b99e4d6b945a71d19e0b756
tahmid888 <76845333+tahmid888@users.noreply.github.com>
2022-02-06 22:03:01 +0600

    Add files via upload

---
+++ 228A - Is your horseshoe on the other hoof.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int a[4],i;
+  int  count=0;
+
+   for(int i=0;i<4;i++)
+   {
+       cin>>a[i];}
+       sort(a,a+4);
+
+     for(int i=0;i<3;i++)
+   {
+       if(a[i]==a[i+1])
+    count++;
+   }
+
+cout<<count<<endl;
+    return 0;
+}
