83d54d251d864170a78bb360d3d69194dae2701a
Tahamidul Haque <tahmidulanon@gmail.com>
2022-05-10 23:16:05 +0600

    Add files via upload

---
+++ 1196A - Three Piles of Candies.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main(){
+
+ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    {
+    long long  a,b,c;
+  cin>>a>>b>>c;
+
+      cout<<((a+b+c)/2)<<endl;
+
+      }
+
+  return 0;
+}
